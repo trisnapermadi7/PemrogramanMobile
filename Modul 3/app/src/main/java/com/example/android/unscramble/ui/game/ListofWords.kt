1ef5c172af2f9e94861ec5bb6913a24102759da0
trisnapermadi7 <trisnapermadi7@gmail.com>
2024-06-10 23:36:10 +0800

    modul 3

---
+++ Modul 3/app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.unscramble.ui.game
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+// List with all the words for the Game
+val allWordsList: List<String> =
+    listOf("rem", "ram", "subaru", "emilia", "rin", "shirou", "gilgamesh", "saber", "petra",
+        "reinhard", "crusch", "frederica", "beatrice", "felix", "irisviel", "kiritsugu", "illyasviel",
+        "shinji", "zouken", "taiga", "sakura", "medusa", "roswaal", "wilhelm", "priscilla", "echidna", "satella",
+        "shiroko", "tsurugi", "yuuka", "chinatsu", "shimiko", "hoshino", "shizumu", "mashiro", "chihiro", "haruna")
